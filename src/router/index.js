d227915d10da1b8b9813fe4145afb94668b3c4b1
Hannah Newsome <h.e.newsome@gmail.com>
2022-12-31 21:30:41 -0700

    adding auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Dashboard from '../views/Dashboard.vue';
+import EditStarterProfile from '../views/Starters/EditStarterProfile.vue';
+import StarterProfile from '../views/Starters/StarterProfile.vue';
 
 const routes = [
   {
@@ -14,7 +19,37 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+},
+{
+    path: '/register',
+    name: 'Register',
+    component: Register
+},
+{
+    path: '/',
+    name: 'Dashboard',
+    component: Dashboard
+},
+{
+  path: '/login',
+  name: 'login',
+  component: Login
+},
+{
+  path: '/profile',
+  name: 'Profile',
+  component: StarterProfile
+},
+{
+  path: '/edit',
+  name: 'EditProfile',
+  component: EditStarterProfile
+}
 ]
 
 const router = createRouter({
